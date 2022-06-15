f10dbedd11fe685789645205e0e3a66f899ade97
Hemachandra S <97468545+HemachandraS@users.noreply.github.com>
2022-06-15 16:40:30 +0530

    Update ProductReducer.js

--- e-commerce front-end/src/redux/ProductReducer/ProductReducer.js
+++ e-commerce front-end/src/redux/ProductReducer/ProductReducer.js
@@ -12,3 +12,19 @@ export const ProductReducer = (store = init, { type, payload }) => {
       return store;
   }
 };
+// ===================================================================
+export const getProductDataFilterSort = (val) => async (dispatch) => {
+  console.log(val);
+  axios
+    .get(
+      `https://entities-01.herokuapp.com/product?${
+        val.color ? `color=${val.color}` : ""
+      }&${val.size ? `size=${val.size}` : ""}&${
+        val.sort ? `sort=${val.sort}` : ""
+      }&${val.page ? `page=${val.page}` : ""}&${
+        val.perPage ? `perPage=${val.perPage}` : ""
+      }`
+    )
+    .then((res) => dispatch(addData(res.data)))
+    .catch((error) => console.log(error));
+};
