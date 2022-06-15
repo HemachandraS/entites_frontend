ee66f665d064460bdd9ee5ae14a88d7b61a339f1
Hemachandra S <97468545+HemachandraS@users.noreply.github.com>
2022-06-15 16:39:49 +0530

    Update ProductAction.js

--- e-commerce front-end/src/redux/ProductReducer/ProductAction.js
+++ e-commerce front-end/src/redux/ProductReducer/ProductAction.js
@@ -11,18 +11,3 @@ export const getProductData = (val) => async (dispatch) => {
     .then((res) => dispatch(addData(res.data)))
     .catch((error) => console.log(error));
 };
-export const getProductDataFilterSort = (val) => async (dispatch) => {
-  console.log(val);
-  axios
-    .get(
-      `https://simple-store-01.herokuapp.com/product?${
-        val.color ? `color=${val.color}` : ""
-      }&${val.size ? `size=${val.size}` : ""}&${
-        val.sort ? `sort=${val.sort}` : ""
-      }&${val.page ? `page=${val.page}` : ""}&${
-        val.perPage ? `perPage=${val.perPage}` : ""
-      }`
-    )
-    .then((res) => dispatch(addData(res.data)))
-    .catch((error) => console.log(error));
-};
