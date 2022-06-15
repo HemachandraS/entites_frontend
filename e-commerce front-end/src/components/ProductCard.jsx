7b4332990d337081343f6df431a2123b0ddbb972
Hemachandra S <97468545+HemachandraS@users.noreply.github.com>
2022-06-15 16:37:54 +0530

    Update ProductCard.jsx

--- e-commerce front-end/src/components/ProductCard.jsx
+++ e-commerce front-end/src/components/ProductCard.jsx
@@ -12,18 +12,18 @@ export const ProductCard = ({ props }) => {
     <div className="product-card">
       <img src={product_image[0]} />
 
-      <div className="field">
-        <div className="title">Name:-</div>
-        <div>{product_title}</div>
-      </div>
+            <div className="field">
+              <div className="title">Name:-</div>
+              <div>{product_title}</div>
+            </div>
       <div className="field">
         <div className="title">Color:-</div>
         <div>{product_color[0]}</div>
       </div>
-      <div className="field">
-        <div className="title">Size:-</div>
-        <div>{product_size[0]}</div>
-      </div>
+                <div className="field">
+                  <div className="title">Size:-</div>
+                  <div>{product_size[0]}</div>
+                </div>
       <div className="field">
         <div className="title">Price:-</div>
         <div> ₹{product_price}</div>
