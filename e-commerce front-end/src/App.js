2d6065df390728e3188533b12f1db589662947ba
Hemachandra S <97468545+HemachandraS@users.noreply.github.com>
2022-06-15 16:33:25 +0530

    Update App.js

--- e-commerce front-end/src/App.js
+++ e-commerce front-end/src/App.js
@@ -1,14 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { ProductPage } from "./components/ProductPage";
-import { FilterSort } from "./components/FilterSort";
+import { ProPage } from "./components/ProductPage";
+import { FilSort } from "./components/FilterSort";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <FilterSort />
-      <ProductPage />
+      <FilSort />
+      <ProPage />
 
     </div>
   );
